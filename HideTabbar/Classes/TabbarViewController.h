83a709ae94077ba850cd340e3036c8f3c04f369f
Shang Zhao <xaduke@gmail.com>
2011-03-28 22:44:12 +0800

    Add infragstructure

---
+++ HideTabbar/Classes/TabbarViewController.h
@@ -0,0 +1,16 @@
+//
+//  TabbarViewController.h
+//  HideTabbar
+//
+//  Created by Worker on 3/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TabbarViewController : UITabBarController {
+
+}
+
+@end
