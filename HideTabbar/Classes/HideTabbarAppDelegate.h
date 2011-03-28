fb1745e4a888aa65c9296ecfdc56f64cec16396e
Shang Zhao <xaduke@gmail.com>
2011-03-28 22:18:22 +0800

    Add Hide Tab bar feature POT

---
+++ HideTabbar/Classes/HideTabbarAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HideTabbarAppDelegate.h
+//  HideTabbar
+//
+//  Created by Worker on 3/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HideTabbarAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
